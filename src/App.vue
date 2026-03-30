8436a64b75ec20ce1991a5e103e6466ef4d6b843
alisagerasimova <mixeeva3316@mail.ru>
2026-03-30 13:06:36 +0300

    согласовали эффект жидкого стекла

--- src/App.vue
+++ src/App.vue
@@ -446,16 +446,10 @@ body {
     background-color: #2f2f2f;
 
 
-    //background: rgba(255, 255, 255, 0.2);
-    //backdrop-filter: blur(10px);
-    //-webkit-backdrop-filter: blur(10px);
-    //box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-    //border: 2px solid rgba(255, 255, 255, 0.18);
-    /*
-        Эффект жидкого стекла
-        Оставил для обсуждения
-        Не удалять!
-    */
+    background: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(5px);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    border: 2px solid rgba(255, 255, 255, 0.18);
 }
 
 .actions svg {
--- src/components/MyDropMenu.vue
+++ src/components/MyDropMenu.vue
@@ -152,17 +152,6 @@ h1 {
     overflow-y: scroll;
     overflow: auto;
     border-right: white 2px solid;
-
-    //background: rgba(255, 255, 255, 0.2);
-    //backdrop-filter: blur(10px);
-    //-webkit-backdrop-filter: blur(10px);
-    //box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-    //border-right: 2px solid rgba(255, 255, 255, 0.18);
-    /*
-        Эффект жидкого стекла
-        Оставил для обсуждения
-        Не удалять!
-    */
 }
 
 /* Затемнение фона */

05b582460821e5f407a32e6b9056dcfd0f33bb5d
alisagerasimova <alisa.gerasimova2008@mail.ru>
2026-03-31 18:44:55 +0300

    кастомиз-а иконку на ехе

--- forge.config.js
+++ forge.config.js
@@ -4,12 +4,15 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: './mainicon',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        setupIcon: './mainicon.ico',
+      },
     },
     {
       name: '@electron-forge/maker-zip',
